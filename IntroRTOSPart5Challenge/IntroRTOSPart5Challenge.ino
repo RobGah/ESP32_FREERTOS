ddbeee23fdf71aaf6a6d53d385200254c994a37c
RobGah <47264017+RobGah@users.noreply.github.com>
2021-06-26 15:18:21 -0400

    Challenge 5 complete

--- IntroRTOSPart5Challenge/IntroRTOSPart5Challenge.ino
+++ IntroRTOSPart5Challenge/IntroRTOSPart5Challenge.ino
@@ -1,7 +1,21 @@
 /**
  * Digikey Intro to FreeRTOS w/ Shawn Hymel
- * Challenge 4: Using Queues
+ * Challenge 5: Using Queues
  * Rob Garrone
+ * 
+ * NOTES: 
+ * My solution is slightly different but equivalent to Shawn's. He uses a struct
+ * to capture messages, whereas I just go w/ plain old strings. I did peak at the
+ * solution Shawn came up with after I got mine to a "workable" state, and cleaned
+ * some stuff up based on his solution.
+ * 
+ * Description:
+ * One task performs basic echo on Serial. If it sees "delay" followed by a
+ * number, it sends the number (in a queue) to the second task. If it receives
+ * a message in a second queue, it prints it to the console. The second task
+ * blinks an LED. When it gets a message from the first queue (number), it
+ * updates the blink delay to that number. Whenever the LED blinks 100 times,
+ * the second task sends a message to the first task to be printed.
  */
 
 // Use only core 1 for demo purposes
@@ -12,62 +26,87 @@ static const BaseType_t app_cpu = 1;
 #endif
 
 //Settings
-static const uint8_t queue_len = 8;
+static const uint8_t queue_len = 5;
 static const int led_pin = LED_BUILTIN; 
+static const char command[] = "delay";
+static const uint8_t commandlen = strlen(command);
+static const uint8_t userbuffersize = 255;
+static const uint8_t stringbuffersize =25;
+
 
 //global variables
 static QueueHandle_t AtoB_queue;
 static QueueHandle_t BtoA_queue;
-static int blinkval =0;
-static uint8_t blinkcount =0;
-static volatile bool printFlag = 0;
-void * msgptr = NULL;
 
 //Listen Task
 void taskA(void *parameter)
 {
-  char bstring[10];
+  char fromBstring[stringbuffersize];
+  char toBstring[stringbuffersize];
   char c;
-  const uint8_t arrsize = 255;
-  char userinput[arrsize];
+  char clicommand[stringbuffersize];
+  char userinput[userbuffersize];
   uint8_t idx = 0;
+
+  //clear all buffers
+  memset(userinput,0,userbuffersize);
+  memset(fromBstring,0,stringbuffersize);
+  memset(toBstring,0,stringbuffersize);
 
   while(1)
   {
     //If B gives us something, print it!
-    if(xQueueReceive(BtoA_queue,(void *)&bstring,0) == pdTRUE)
+    if(xQueueReceive(BtoA_queue,(void *)&fromBstring,0) == pdTRUE)
     {
-      Serial.println(bstring);
+      Serial.print("Received from Task B: ");
+      Serial.println(fromBstring);
     }
     
     //read user input
     if(Serial.available()>0)
     {
       c = Serial.read();
-      if(idx<arrsize-1)
+      if(idx<userbuffersize-1)
       {
         userinput[idx] = c;        
         idx++;
+      } 
+      if((c=='\n')|| (c=='\r'))
+      {       
+        Serial.println();         
+        //parse user input for a command and a delay value
+        sscanf(userinput,"%s %s", &clicommand, &toBstring);
+        //Serial.print("Command is: ");
+        //Serial.println(clicommand);
+        //Serial.print("Delay val is: ");
+        //Serial.println(toBstring);
+        //Serial.print("strcmp returns ");
+        //Serial.println(strcmp(clicommand,command));
+        if(strcmp(clicommand,command) == 0)
         
-        if(c=='\n')
+        //could also do this:
+        //if(memcmp(clicommand,command,commandlen)==0)
         {
-          userinput[idx-1] == '\0';
-          
-          
-          int delayval;
-          char command [20];
-          //parse user input for a command and a delay value
-          sscanf(userinput,"%s %d", command, &delayval);
-
-          if(command =="delay")
-          {            
+          //check if the delay value is a number
+          if(isDigit(toBstring[0]))
+            {            
             //send our msg, wait 10 ticks
             //I don't think we'd do anything differently if it WASN'T successful sending
-            xQueueSend(AtoB_queue,(void *)&delayval,10);
- 
-            idx=0;
+            xQueueSend(AtoB_queue,(void *)&toBstring,10);
           }
+          memset(userinput,0,userbuffersize);
+          idx=0;
         }
+        else
+        {
+          //clear userinput buffer to accept another read
+          memset(userinput,0,userbuffersize);
+          idx=0;
+        }
+      }
+      else
+      {
+        Serial.print(c);
       }
     }
   }
@@ -75,48 +114,44 @@ void taskA(void *parameter)
 
 void taskB(void *parameter)
 {
-  char astring[] = "Blinked!";
+  //setup
+  char toAstring[stringbuffersize];
+  char fromAstring[stringbuffersize];
+  int blinkcount = 0;  
+  int blinkint = 500; 
+
+  memset(toAstring,0,stringbuffersize);
+  memset(fromAstring,0,stringbuffersize);
+
+
   
   while(1)
   {
     //update blinkval based on A's sent data
-    xQueueReceive(AtoB_queue,(void *)&blinkval,0);
-
-    Serial.print("Current blink rate is ");
-    Serial.println(blinkval);
+    if(xQueueReceive(AtoB_queue,(void *)&fromAstring,0)==pdTRUE)
+    {
+      blinkint = atoi(fromAstring);
+      blinkint = abs(blinkint);
+      strcpy(toAstring,fromAstring);
+      strcat(toAstring," ms delay applied!");
+      xQueueSend(BtoA_queue,(void *)toAstring,10);
+    }
+  
     //blink!
     digitalWrite(led_pin,HIGH);
-    vTaskDelay(blinkval / portTICK_PERIOD_MS);
+    vTaskDelay(blinkint / portTICK_PERIOD_MS);
     digitalWrite(led_pin,LOW);
-    vTaskDelay(blinkval / portTICK_PERIOD_MS);
+    vTaskDelay(blinkint / portTICK_PERIOD_MS);
     //every blink increments the count
     blinkcount++;
   
     if(blinkcount == 100)
     {
-      xQueueSend(BtoA_queue,(void*)astring,10);
+      strcpy(toAstring,"Blinked 100 Times!");
+      xQueueSend(BtoA_queue,(void*)&toAstring,10);
+      memset(toAstring,0,stringbuffersize);
+      blinkcount = 0;
     }
-  }
-}
-
-void printMessage(void *parameter)
-{
-  while(1)
-  {
-    if(printFlag==1)
-    {
-
-      //FIX THIS! WE need to always have msgptr be a char pointer!
-      Serial.println((char*)msgptr); //print message
-
-      // Give amount of free heap memory (uncomment if you'd like to see it)
-      //Serial.print("Free heap (bytes): ");
-      //Serial.println(xPortGetFreeHeapSize());
-      
-      vPortFree(msgptr);
-      msgptr = NULL;
-      printFlag = 0; //reset printFlag
-      }
   }
 }
 
@@ -132,7 +167,13 @@ void setup()
   //show time!
   Serial.println();
   Serial.println("---------------------------------");
-  Serial.println("Program will echo what is given:");
+  Serial.println("Enter delay followed by a time in ms e.g. \"delay 500\":");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  AtoB_queue = xQueueCreate(queue_len,sizeof(int));
+  BtoA_queue = xQueueCreate(queue_len,25*sizeof(char));
+  
 
   xTaskCreatePinnedToCore(    //use xTaskCreate() in vanilla FREERTOS
     taskA,              //our task function
@@ -146,15 +187,6 @@ void setup()
     xTaskCreatePinnedToCore(    //use xTaskCreate() in vanilla FREERTOS
     taskB,              //our task function
     "Task B",           // name of task
-    1024,                   //Stack size (bytes in ESP32,words in FREERTOS
-    NULL,                   //Parameter to pass to function
-    1,                      //priority (higher=greater priority, 0 to configMAX_PRIORITIES-1)
-    NULL,                   //Task Handle
-    app_cpu);               //Run on one core for demo purposes
-
-    xTaskCreatePinnedToCore(    //use xTaskCreate() in vanilla FREERTOS
-    printMessage,              //our task function
-    "print Message",           // name of task
     1024,                   //Stack size (bytes in ESP32,words in FREERTOS
     NULL,                   //Parameter to pass to function
     1,                      //priority (higher=greater priority, 0 to configMAX_PRIORITIES-1)
